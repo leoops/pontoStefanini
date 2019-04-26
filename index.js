881daef1c5c41002adef16779346e7c1d2663236
Leonardo Pereira <leonardopereira519@gmail.com>
2019-04-25 23:36:40 -0300

    Inclusão de bibliotecas

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import { AppRegistry } from 'react-native';
 import Routes from './src/routes';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Routes);
